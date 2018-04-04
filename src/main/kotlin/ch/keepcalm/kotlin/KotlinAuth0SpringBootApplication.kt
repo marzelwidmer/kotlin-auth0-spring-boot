9194c43539b751214748cb0fc619d18c70a6d2b2
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-04 21:24:51 +0200

    first commit

---
+++ src/main/kotlin/ch/keepcalm/kotlin/KotlinAuth0SpringBootApplication.kt
@@ -0,0 +1,11 @@
+package ch.keepcalm.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAuth0SpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinAuth0SpringBootApplication>(*args)
+}
